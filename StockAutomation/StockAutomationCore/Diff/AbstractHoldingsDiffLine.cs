90dc3cd7563b1111ec3fecd313b9cbb4a7f61b73
Jakub Maloštík <xmalost@fi.muni.cz>
2024-04-05 22:24:01 +0200

    Restructure and add ToText()

--- StockAutomation/StockAutomationCore/Diff/AbstractHoldingsDiffLine.cs
+++
@@ -1,7 +0,0 @@
-namespace StockAutomationCore.Diff;
-
-public abstract class AbstractHoldingsDiffLine
-{
-    public abstract string CompanyName { get; }
-    public abstract string Ticker { get; }
-}
--- StockAutomation/StockAutomationCore/Diff/HoldingsDiff.cs
+++ StockAutomation/StockAutomationCore/Diff/HoldingsDiff.cs
@@ -1,51 +1,80 @@
+using System.Text;
+
 namespace StockAutomationCore.Diff;
 
 public class HoldingsDiff
 {
-    private Dictionary<string, AbstractHoldingsDiffLine> _holdingsDiffLines;
+    private Dictionary<string, HoldingsDiffLine> _holdingsDiffLines;
 
     public HoldingsDiff(IEnumerable<HoldingsSnapshotLine> oldHoldings, IEnumerable<HoldingsSnapshotLine> newHoldings)
     {
         _holdingsDiffLines = CalculateDiff(oldHoldings, newHoldings);
     }
 
-    public HoldingsDiff(ICollection<AbstractHoldingsDiffLine> holdingsDiffs)
+    public HoldingsDiff(ICollection<HoldingsDiffLine> holdingsDiffs)
     {
         ArgumentNullException.ThrowIfNull(holdingsDiffs);
 
         _holdingsDiffLines = holdingsDiffs.ToDictionary(h => h.Ticker);
     }
 
-    private static Dictionary<string, AbstractHoldingsDiffLine> CalculateDiff(IEnumerable<HoldingsSnapshotLine> oldHoldings, IEnumerable<HoldingsSnapshotLine> newHoldings)
+    private static Dictionary<string, HoldingsDiffLine> CalculateDiff(IEnumerable<HoldingsSnapshotLine> oldHoldings,
+        IEnumerable<HoldingsSnapshotLine> newHoldings)
     {
         var oldd = oldHoldings.ToDictionary(hl => hl.Ticker);
         var newd = newHoldings.ToDictionary(hl => hl.Ticker);
         var tickers = oldd.Keys.Union(newd.Keys);
         return tickers.ToDictionary(
             t => t,
-            t => GetDiffLine(oldd.GetValueOrDefault(t), newd.GetValueOrDefault(t))
+            t => new HoldingsDiffLine(oldd.GetValueOrDefault(t), newd.GetValueOrDefault(t))
         );
     }
 
-    private static AbstractHoldingsDiffLine GetDiffLine(HoldingsSnapshotLine? old, HoldingsSnapshotLine? @new)
+    private static StringBuilder GetTextSection(string newLine, string title, List<string> lines)
     {
-        if (old != null && @new != null)
-        {
-            return new HoldingsDiffLine(old.Value, @new.Value);
-        }
-        if (old != null)
-        {
-            return new HoldingsDiffLineOld(old.Value);
-        }
-        if (@new != null)
-        {
-            return new HoldingsDiffLineNew(@new.Value);
-        }
-        throw new InvalidOperationException("Both old and new cannot be null");
+        var result = new StringBuilder();
+        if (lines.Count == 0) return result;
+        result.Append(title);
+        result.Append(newLine);
+        result.AppendJoin(newLine, lines);
+        return result;
     }
 
-    public string ToText()
+    public string ToText(string newLine = "\r\n")
     {
-        throw new NotImplementedException("ToText is not yet implemented");
+        var newPositions = _holdingsDiffLines.Values
+            .Where(hdl => hdl.Old.Quantity == 0)
+            .OrderBy(hdl => hdl.CompanyName)
+            .Select(hdl => hdl.GetFormattedString())
+            .ToList();
+        var increasedPositions = _holdingsDiffLines.Values
+            .Where(hdl => hdl.Old.Quantity > 0 && hdl.QuantityDiff > 0)
+            .OrderBy(hdl => hdl.CompanyName)
+            .Select(hdl => hdl.GetFormattedString())
+            .ToList();
+        var reducedPositions = _holdingsDiffLines.Values
+            .Where(hdl => hdl.QuantityDiff < 0)
+            .OrderBy(hdl => hdl.CompanyName)
+            .Select(hdl => hdl.GetFormattedString())
+            .ToList();
+
+        if (newPositions.Count == 0 && increasedPositions.Count == 0 && reducedPositions.Count == 0)
+        {
+            return $"No changes in the index";
+        }
+        List<StringBuilder> sections =
+        [
+            GetTextSection(newLine, "New positions:", newPositions),
+            GetTextSection(newLine, "Increased positions:", increasedPositions),
+            GetTextSection(newLine, "Reduced positions:", reducedPositions)
+        ];
+        var result = new StringBuilder();
+        result.AppendJoin(newLine + newLine, sections);
+        return result.ToString();
+    }
+
+    public override string ToString()
+    {
+        return ToText(Environment.NewLine);
     }
 }
