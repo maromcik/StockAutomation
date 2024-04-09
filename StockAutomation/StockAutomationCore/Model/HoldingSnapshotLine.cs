04dcdc0c1934e12958e1214a68c168f2c1875903
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-04-09 22:44:24 +0200

    clean(refactor)

--- StockAutomation/StockAutomationCore/Model/HoldingSnapshotLine.cs
+++ StockAutomation/StockAutomationCore/Model/HoldingSnapshotLine.cs
@@ -10,11 +10,11 @@ public readonly struct HoldingSnapshotLine
     public string Ticker { get; }
     public string Cusip { get; }
     public BigInteger Shares { get; }
-    public decimal MarketValueUSD { get; }
+    public decimal MarketValueUsd { get; }
     public decimal Weight { get; }
 
     private HoldingSnapshotLine(DateTime date, string fund, string companyName, string ticker, string cusip,
-        BigInteger shares, decimal marketValueUSD, decimal weight)
+        BigInteger shares, decimal marketValueUsd, decimal weight)
     {
         Date = date;
         Fund = fund;
@@ -22,7 +22,7 @@ public readonly struct HoldingSnapshotLine
         Ticker = ticker;
         Cusip = cusip;
         Shares = shares;
-        MarketValueUSD = marketValueUSD;
+        MarketValueUsd = marketValueUsd;
         Weight = weight;
     }
 
@@ -34,15 +34,15 @@ public readonly struct HoldingSnapshotLine
         Ticker = ticker;
         Cusip = cusip;
         Shares = 0;
-        MarketValueUSD = 0;
+        MarketValueUsd = 0;
         Weight = 0;
     }
 
     public static HoldingSnapshotLine Create(DateTime date, string fund, string company, string ticker, string cusip,
-        BigInteger shares, decimal marketValueUSD, decimal weight)
+        BigInteger shares, decimal marketValueUsd, decimal weight)
     {
         // todo assertions
-        return new HoldingSnapshotLine(date, fund, company, ticker, cusip, shares, marketValueUSD, weight);
+        return new HoldingSnapshotLine(date, fund, company, ticker, cusip, shares, marketValueUsd, weight);
     }
 
     public static HoldingSnapshotLine CreateDefaultFrom(HoldingSnapshotLine other)
@@ -53,6 +53,6 @@ public readonly struct HoldingSnapshotLine
     public override string ToString()
     {
         return
-            $"HoldingSnapshotLine({Date}, {Fund}, {CompanyName}, {Ticker}, {Cusip}, {Shares}, {MarketValueUSD}, {Weight})";
+            $"HoldingSnapshotLine({Date}, {Fund}, {CompanyName}, {Ticker}, {Cusip}, {Shares}, {MarketValueUsd}, {Weight})";
     }
 }
--- StockAutomation/StockAutomationCore/Parser/HoldingSnapshotLineParser.cs
+++ StockAutomation/StockAutomationCore/Parser/HoldingSnapshotLineParser.cs
@@ -9,10 +9,8 @@ public static class HoldingSnapshotLineParser
 {
     public static IEnumerable<HoldingSnapshotLine> ParseLines(string path)
     {
-        using var lineParser = new TextFieldParser(new FileStream(path, FileMode.Open, FileAccess.Read))
-        {
-            HasFieldsEnclosedInQuotes = true
-        };
+        using var lineParser = new TextFieldParser(new FileStream(path, FileMode.Open, FileAccess.Read));
+        lineParser.HasFieldsEnclosedInQuotes = true;
         lineParser.SetDelimiters(",");
 
         lineParser.ReadLine(); // skip header
@@ -20,16 +18,16 @@ public static class HoldingSnapshotLineParser
         {
             var fields = lineParser.ReadFields() ?? throw new InvalidOperationException("parser returned null");
 
-            if (!DateTime.TryParseExact(fields[0], "MM/dd/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date)) yield break;
-            string fund = fields[1];
-            string companyName = fields[2].Replace("\"", "");
-            string ticker = fields[3].Replace("\"", "");
-            string cusip = fields[4];
-            BigInteger shares = BigInteger.Parse(fields[5].Replace(",", ""), CultureInfo.InvariantCulture);
-            decimal marketValueUSD = decimal.Parse(fields[6].Replace("$", "").Replace(",", ""), CultureInfo.InvariantCulture);
-            decimal weight = decimal.Parse(fields[7].Replace("%", ""), CultureInfo.InvariantCulture) / 100;
+            if (!DateTime.TryParseExact(fields[0], "MM/dd/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out var date)) yield break;
+            var fund = fields[1];
+            var companyName = fields[2].Replace("\"", "");
+            var ticker = fields[3].Replace("\"", "");
+            var cusip = fields[4];
+            var shares = BigInteger.Parse(fields[5].Replace(",", ""), CultureInfo.InvariantCulture);
+            var marketValueUsd = decimal.Parse(fields[6].Replace("$", "").Replace(",", ""), CultureInfo.InvariantCulture);
+            var weight = decimal.Parse(fields[7].Replace("%", ""), CultureInfo.InvariantCulture) / 100;
 
-            var line = HoldingSnapshotLine.Create(date, fund, companyName, ticker, cusip, shares, marketValueUSD, weight);
+            var line = HoldingSnapshotLine.Create(date, fund, companyName, ticker, cusip, shares, marketValueUsd, weight);
 
             yield return line;
         }
