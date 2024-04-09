4b7f5daf4476644dc4dc2d3a0d2a8172121b982f
Lukáš Chudíček <xchudic1@fi.muni.cz>
2024-04-09 12:40:57 +0200

    fix(parser): parser should be able to support both agreed formats

--- StockAutomation/StockAutomationCore/Parser/HoldingSnapshotLineParser.cs
+++ StockAutomation/StockAutomationCore/Parser/HoldingSnapshotLineParser.cs
@@ -22,12 +22,15 @@ public static partial class HoldingSnapshotLineParser
 
         string date = match.Groups[1].Value;
         string fund = match.Groups[2].Value;
-        string company = match.Groups[3].Value;
-        string ticker = match.Groups[4].Value;
-        string cusip = match.Groups[5].Value;
-        string shares = match.Groups[6].Value;
-        string marketValueUSD = match.Groups[7].Value;
-        string weight = match.Groups[8].Value;
+        string _company_fst = match.Groups[4].Value;
+        string company = _company_fst == "" ? match.Groups[5].Value : _company_fst;
+        string _ticker_fst = match.Groups[7].Value;
+        string ticker = _ticker_fst == "" ? match.Groups[8].Value : _ticker_fst;
+        string cusip = match.Groups[9].Value;
+        string _shares_fst = match.Groups[11].Value;
+        string shares = _shares_fst == "" ? match.Groups[12].Value : _shares_fst;
+        string marketValueUSD = match.Groups[13].Value;
+        string weight = match.Groups[14].Value;
 
         if (!DateTime.TryParseExact(date, "MM/dd/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None,
                 out DateTime properDate)) return null;
@@ -45,16 +48,12 @@ public static partial class HoldingSnapshotLineParser
         );
     }
 
-    [GeneratedRegex(@"^(\d{2}/\d{2}/\d{4}),([^,]+),""([^""]+)"",([^,]*),([^,]+),""([^""]+)"",""\$([^""]+)"",([^%]+)%$")]
+    [GeneratedRegex(@"^(\d{2}/\d{2}/\d{4}),([^,]+),(""([^,]+)""|([^,]+)),(""([^""]*)""|([^,]*)),([^,]+),(""([^"".]+)""|([^,]+)),""\$(.+)"",([^%]+)%$")]
     private static partial Regex TheLineRegex();
 
     public static IEnumerable<HoldingSnapshotLine> ParseLines(string filepath)
-    {
-        return File.ReadLines(filepath)
+        => File.ReadLines(filepath)
             .Skip(1) /* ingnore header */
             .SkipLast(1) /* ignore whatever that is */
-            .Select(Parse)
-            .Where(hsl => hsl.HasValue) // TODO Don't just ignore invalid lines
-            .Select(hsl => hsl!.Value);
-    }
+            .Select(line => Parse(line) ?? throw new Exception($"Could not parse line: {line}"));
 }
