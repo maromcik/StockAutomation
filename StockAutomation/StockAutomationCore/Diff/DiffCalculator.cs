c007a70dca6de6c389e171b83a435f77d95bb0f2
Jakub Maloštík <xmalost@fi.muni.cz>
2024-04-09 16:15:04 +0200

    Add newline configuration for GetDiffText

--- StockAutomation/StockAutomationCore/Diff/DiffCalculator.cs
+++ StockAutomation/StockAutomationCore/Diff/DiffCalculator.cs
@@ -4,11 +4,11 @@ namespace StockAutomationCore.Diff;
 
 public static class DiffCalculator
 {
-    public static string GetDiffText(string pathOld, string pathNew)
+    public static string GetDiffText(string pathOld, string pathNew, string newLine = "\r\n")
     {
         var old = HoldingSnapshotLineParser.ParseLines(pathOld);
         var @new = HoldingSnapshotLineParser.ParseLines(pathNew);
         var hd = new HoldingsDiff(old, @new);
-        return hd.ToText();
+        return hd.ToText(newLine);
     }
 }
