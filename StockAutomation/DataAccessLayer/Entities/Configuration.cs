eb3a736f880752f33d5b6dab6a1b078db5af1453
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-04-27 19:20:28 +0200

    fix(config): download url changed to nullable

--- StockAutomation/DataAccessLayer/Entities/Configuration.cs
+++ StockAutomation/DataAccessLayer/Entities/Configuration.cs
@@ -3,7 +3,7 @@ namespace DataAccessLayer.Entities;
 public class Configuration : BaseEntity
 {
     public required string SnapshotDir { get; set; }
-    public required string DownloadUrl { get; set; }
+    public string? DownloadUrl { get; set; }
 
     public required OutputFormat OutputFormat { get; set; } = OutputFormat.HTML;
 }
