4fd57e48f03c95d6292a342e615417dd5ad9af12
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-05-02 00:35:08 +0200

    fix(cli): fixed appsettings

--- StockAutomation/StockAutomationClient/ApiConnector/ApiConfiguration.cs
+++ StockAutomation/StockAutomationClient/ApiConnector/ApiConfiguration.cs
@@ -1,9 +1,9 @@
-using StockAutomationCore.Configuration;
+using StockAutomationClient.Configuration;
 
 namespace StockAutomationClient.ApiConnector;
 
 public static class ApiConfiguration
 {
     public static readonly string ApiUri = StockAutomationConfig.GetSection("Api")["defaultUrl"] ??
-                                           "https://stock-automation.dyn.cloud.e-infra.cz/api";
+                                           "http://localhost:5401";
 }
