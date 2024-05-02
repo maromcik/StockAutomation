b826cf53d7fd9697e62367a0bb8ad147087637ba
Lukáš Chudíček <xchudic1@fi.muni.cz>
2024-05-02 11:08:23 +0200

    test: snapshot service tested

--- StockAutomation/BusinessLayer.Test/SnapshotService.cs
+++ StockAutomation/BusinessLayer.Test/SnapshotService.cs
@@ -62,4 +62,136 @@ public class SnapshotServiceTests
 
         Assert.That((await service.DownloadSnapshotAsync()).IsOk);
     }
+
+    [Test]
+    public async Task GetSnapshotsAsync_DownloadedSnapshot_ReturnsThatSnapshot()
+    {
+        // Arrange
+        var context = new StockAutomationDbContext(_options);
+        var service = new SnapshotService<DownloaderMock>(context, new HttpClient());
+        await service.DownloadSnapshotAsync();
+
+        // Act
+        var snapshots = await service.GetSnapshotsAsync();
+
+        // Assert
+        Assert.That(snapshots.Count(), Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task DeleteSnapshotsAsync_TargetSingleExistingEntry_DeletesThatEntry()
+    {
+        // Arrange
+        var context = new StockAutomationDbContext(_options);
+        var service = new SnapshotService<DownloaderMock>(context, new HttpClient());
+        await service.DownloadSnapshotAsync();
+        var snapshots = await service.GetSnapshotsAsync();
+        var snapshot = snapshots.First();
+
+        // Act
+        await service.DeleteSnapshotsAsync([snapshot.Id]);
+        var snapshotsAfter = await service.GetSnapshotsAsync();
+
+        // Assert
+        Assert.That(snapshotsAfter.Count(), Is.EqualTo(0));
+    }
+
+    // todo inconsistant behavior of `DeleteSnapshotsAsync`
+    //  - would expect either both of DeleteSnapshotsAsync_TargetOneExistingAndOneNonExistingEntry_DeletesExistingAndReturnsOk
+    //    and DeleteSnapshotsAsync_TargetSingleNonExistingEntry_ReturnsOk to fail or both of them to succeed
+
+    [Test]
+    public async Task DeleteSnapshotsAsync_TargetOneExistingAndOneNonExistingEntry_DeletesExistingAndReturnsOk()
+    {
+        // Arrange
+
+        var context = new StockAutomationDbContext(_options);
+        var service = new SnapshotService<DownloaderMock>(context, new HttpClient());
+        await service.DownloadSnapshotAsync();
+        var snapshots = await service.GetSnapshotsAsync();
+        var snapshot = snapshots.First();
+
+        // Act
+
+        var nonexistentId = 420;
+        Assert.That(snapshots.Any(s => s.Id == nonexistentId), Is.False);
+
+        var result = await service.DeleteSnapshotsAsync([snapshot.Id, nonexistentId]);
+        var snapshotsAfter = await service.GetSnapshotsAsync();
+        Assert.Multiple(() =>
+        {
+
+            // Assert
+            Assert.That(result.IsOk);
+            Assert.That(snapshotsAfter.Count(), Is.EqualTo(0));
+        });
+    }
+
+    // todo either make pass & uncomment or delete
+    // [Test]
+    // public async Task DeleteSnapshotsAsync_TargetSingleNonExistingEntry_ReturnsOk()
+    // {
+    //     // Arrange
+    //     var context = new StockAutomationDbContext(_options);
+    //     var service = new SnapshotService<DownloaderMock>(context, new HttpClient());
+    //     var snapshots = await service.GetSnapshotsAsync();
+    //     var nonexistentId = 420;
+
+    //     // Act
+    //     var result = await service.DeleteSnapshotsAsync([nonexistentId]);
+    //     var snapshotsAfter = await service.GetSnapshotsAsync();
+
+    //     // Assert
+    //     Assert.Multiple(() =>
+    //     {
+    //         Assert.That(result.IsOk);
+    //         Assert.That(snapshotsAfter.Count(), Is.EqualTo(0));
+    //     });
+    // }
+
+    [Test]
+    public async Task DeleteSnapshotsAsync_TargetOneOfExistingEntries_OthersUnaffected()
+    {
+        // Arrange
+
+        var context = new StockAutomationDbContext(_options);
+        var service = new SnapshotService<DownloaderMock>(context, new HttpClient());
+
+        await service.DownloadSnapshotAsync();
+        var snapshotsAfterFirstDownload = await service.GetSnapshotsAsync();
+        var firstSnaposhot = snapshotsAfterFirstDownload.First();
+
+        await service.DownloadSnapshotAsync();
+
+        // Act
+
+        await service.DeleteSnapshotsAsync([firstSnaposhot.Id]);
+        var snapshotsAfter = await service.GetSnapshotsAsync();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(snapshotsAfter.Count(), Is.EqualTo(1));
+            Assert.That(snapshotsAfter.First().Id, Is.Not.EqualTo(firstSnaposhot.Id));
+        });
+    }
+
+    [Test]
+    public async Task CompareSnapshotsAsync_SameSnapshot_IsOk()
+    {
+        // Arrange
+        var context = new StockAutomationDbContext(_options);
+        var service = new SnapshotService<DownloaderMock>(context, new HttpClient());
+        await service.DownloadSnapshotAsync();
+        var snapshots = await service.GetSnapshotsAsync();
+        var snapshot = snapshots.First();
+
+        // Act
+        var result = await service.CompareSnapshotsAsync(snapshot.Id, snapshot.Id);
+
+        Console.WriteLine(result.Value);
+
+        // Assert
+        Assert.That(result.IsOk);
+    }
 }
