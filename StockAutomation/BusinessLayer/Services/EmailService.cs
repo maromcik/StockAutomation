deed625f545ea2707423b2f397e115797b2506ed
Jakub Maloštík <xmalost@fi.muni.cz>
2024-05-09 20:26:37 +0200

    Wtf rider

--- StockAutomation/BusinessLayer/Services/EmailService.cs
+++ StockAutomation/BusinessLayer/Services/EmailService.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Configuration;
 using System.Net;
 using System.Net.Mail;
 using System.Net.Mime;
-using RazorEngine;
 using Encoding = System.Text.Encoding;
 
 namespace BusinessLayer.Services;
