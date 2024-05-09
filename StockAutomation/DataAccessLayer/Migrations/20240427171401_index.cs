6362aafb53e4b125ef085076f9e618e3e830d3ac
Matúš Škvarla <xskvarla@fi.muni.cz>
2024-05-09 14:49:20 +0200

    fix(migrations): added missing migrations

---
+++ StockAutomation/DataAccessLayer/Migrations/20240427171401_index.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class index : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_HoldingSnapshots_DownloadedAt",
+                table: "HoldingSnapshots",
+                column: "DownloadedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_HoldingSnapshots_DownloadedAt",
+                table: "HoldingSnapshots");
+        }
+    }
+}
