6ef4933f6f29189337f4601ea4a18bb80c8cc058
ninarybarova <ninarybarova29@gmail.com>
2024-05-12 13:17:44 +0200

    feat(diff page) - moved snapshots to another page

--- StockAutomation/StockAutomationWeb/Controllers/SnapshotController.cs
+++ StockAutomation/StockAutomationWeb/Controllers/SnapshotController.cs
@@ -4,21 +4,25 @@ using Microsoft.AspNetCore.Mvc;
 namespace StockAutomationWeb.Controllers;
 
 [ApiExplorerSettings(IgnoreApi = true)]
-public class HomeController(
-    ILogger<HomeController> logger,
+public class SnapshotController(
+    ILogger<SnapshotController> logger,
     ISnapshotService snapshotService)
     : BaseController
 {
-    private readonly ILogger<HomeController> _logger = logger;
+    private readonly ILogger<SnapshotController> _logger = logger;
 
     public async Task<IActionResult> Index()
     {
         var snapshots = (await snapshotService.GetSnapshotsAsync()).Take(10).ToList();
         return View(snapshots);
     }
-
-    public IActionResult Privacy()
+    
+    public async Task<IActionResult> Delete(int id)
     {
-        return View();
+        var result = await snapshotService.DeleteSnapshotsAsync([id]);
+        return result.Match(
+            _ => RedirectToAction("Index", "Snapshot"),
+            ErrorView
+        );
     }
 }
